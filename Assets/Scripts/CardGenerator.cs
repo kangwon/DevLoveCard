9542b34e54de3688c7b29c2f226c6221f88832ab
kang_won@naver.com <753951zz>
2020-12-12 21:19:27 +0900

    Implement card movements

--- Assets/Scripts/CardGenerator.cs
+++ Assets/Scripts/CardGenerator.cs
@@ -19,11 +19,13 @@ public class Card
 
 public class CardGenerator : MonoBehaviour
 {
-    public GameObject cardButtonPrefab;
-    public int CardY;
+    public GameObject CardButtonPrefab;
+    public GameObject Canvas;
+    public int CardX = 540;
+    public int CardY = 1737;
     public int CardMargin;
 
-    public List<Card> Cards = new List<Card> 
+    List<Card> CardData = new List<Card> 
     {
         new Card("monster", "몹", "슬라임"),
         new Card("chest", "상자", "보물 상자"),
@@ -31,25 +33,79 @@ public class CardGenerator : MonoBehaviour
         new Card("event", "랜덤", "랜덤 이벤트"),
         new Card("npc", "NPC", "무기 상인"),
     };
+    List<GameObject> CardButtons = new List<GameObject>();
+    int CurrentIndex = 0;
+
+    Vector2 StartPosition;
+    Vector2 EndPosition;
     
     void Start()
     {
-        GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
-        for (int i = 0; i < Cards.Count; i++)
-        {
-            GameObject cardButton = Instantiate(cardButtonPrefab) as GameObject;
-            float cardWidth = cardButton.GetComponent<RectTransform>().rect.width;
-            cardButton.transform.position = 
-                new Vector3(i * (cardWidth + CardMargin), CardY, 1);
-            cardButton.transform.SetParent(canvas.transform, false);
-
-            Card card = Cards[i];
-            cardButton.GetComponentInChildren<Text>().text = card.Slug;
-        }
+        GenerateCardButtons();
+        ArrangeCards();
     }
 
     void Update()
     {
-        
+        // Swipe genture
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartPosition = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            EndPosition = Input.mousePosition;
+            float swipeLen = EndPosition.x - StartPosition.x;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+    }
+
+    void GenerateCardButtons()
+    {
+        for (int i = 0; i < CardData.Count; i++)
+        {
+            GameObject cardButton = Instantiate(CardButtonPrefab) as GameObject;
+            cardButton.transform.SetParent(Canvas.transform, false);
+
+            Card card = CardData[i];
+            cardButton.GetComponentInChildren<Text>().text = card.Slug;
+
+            CardButtons.Add(cardButton);
+        }
+    }
+
+    void ArrangeCards()
+    {
+        for (int i = 0; i < CardButtons.Count; i++)
+        {
+            GameObject cardButton = CardButtons[i];
+            float cardWidth = cardButton.GetComponent<RectTransform>().rect.width;
+            cardButton.transform.position = 
+                new Vector3(CardX + (i - CurrentIndex) * (cardWidth + CardMargin), CardY, 1);
+        }
+    }
+
+    void MoveLeft()
+    {
+        CurrentIndex--;
+        if (CurrentIndex < 0)
+            CurrentIndex = CardButtons.Count - 1;
+        ArrangeCards();
+    }
+
+    void MoveRight()
+    {
+        CurrentIndex++;
+        if (CurrentIndex >= CardButtons.Count)
+            CurrentIndex = 0;
+        ArrangeCards();
     }
 }
